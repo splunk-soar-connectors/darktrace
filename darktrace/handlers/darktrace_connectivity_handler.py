6144fc8d256620cf6056af39581bec232ff2aa5e
Abraham Amselem Ajuelos <abraham.ajuelos@darktrace.com>
2022-07-13 16:57:17 +0200

    darktrace connector

---
+++ darktrace/handlers/darktrace_connectivity_handler.py
@@ -0,0 +1,24 @@
+import phantom.app as phantom
+
+from .darktrace_handler import DarktraceHandler
+
+
+class ConnectivityHandler(DarktraceHandler):
+    def handle_test_connectivity(self):
+        """
+        Handler for the `test_connectivity` action
+        """
+
+        action_status, result = self._client.test_connectivity(self.action_result)
+
+        if phantom.is_fail(action_status):
+            self.save_progress("Test Connectivity Failed")
+            return self.action_result.get_status()
+
+        if not result:
+            message = "Test Connectivity did not return any data in the response"
+            self.save_progress(message)
+            return self.action_result.set_status(phantom.APP_ERROR, message)
+
+        self.save_progress("Test Connectivity passed")
+        return self.action_result.set_status(phantom.APP_SUCCESS)
